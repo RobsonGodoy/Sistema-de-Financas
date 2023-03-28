4f37233a19f546ddb16896a38f7042fc60cc2dbb
Robson Junior <109744930+RobsonGodoy@users.noreply.github.com>
2023-03-28 17:10:12 -0300

    update;

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,5 +3,5 @@ import { Category } from "../types/Category";
 export const categories: Category = {
     food: {title: 'Alimentação', color: 'orange', expense: true},
     rent: {title: 'Aluguel', color: 'red', expense: true,},
-    salary: {title: 'Salary', color:'green', expense: false },
+    salary: {title: 'Receita', color:'green', expense: false },
 }
